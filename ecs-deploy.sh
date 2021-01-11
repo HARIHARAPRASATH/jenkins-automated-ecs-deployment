1a41b1738a0d267d938dafc7e077b1e6e24d6ea0
HARIHARA PRASATH <hari.hari840@gmail.com>
2021-01-11 16:54:39 +0530

    Update ecs-deploy.sh

--- ecs-deploy.sh
+++ ecs-deploy.sh
@@ -8,7 +8,7 @@ CLUSTER=flask-signup-cluster
 FAMILY=`sed -n 's/.*"family": "\(.*\)",/\1/p' taskdef.json`
 NAME=`sed -n 's/.*"name": "\(.*\)",/\1/p' taskdef.json`
 SERVICE_NAME=${NAME}-service
-DESIRED_COUNT="1"
+#DESIRED_COUNT="1"
 env
 aws configure list
 echo $HOME
@@ -21,5 +21,14 @@ aws ecs register-task-definition --family ${FAMILY} --cli-input-json file:///roo
 #Get latest revision
 #REVISION=`aws ecs describe-task-definition --task-definition v1-taskDefintion | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
 REVISION=`aws ecs describe-task-definition --task-definition v1-taskDefintion | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//' | cut -d ","  -f 1`
+#FAILURECHECK
+SERVICES=` aws ecs describe-services  --service ${SERVICE_NAME} --cluster ${CLUSTER} --region REGION ||jq.failures[]`
+#DESIRED COUNT CHECK
+#DESIRED_COUNT=`aws ecs describe-services  --service ${SERVICE_NAME} --cluster ${CLUSTER} --region REGION | jq .services[].desiredCount`
+#Create and update service
+#if["SERVICES"=="];then
+echo"entering to existing service"
+#DESIRED COUNT CHECK
+DESIRED_COUNT=`aws ecs describe-services  --service ${SERVICE_NAME} --cluster ${CLUSTER} --region REGION | jq .services[].desiredCount`
 #aws ecs update-service --cluster ${CLUSTER} --region ${REGION} --service ${SERVICE_NAME} --task-definition ${FAMILY}:${REVISION} --desired-count ${DESIRED_COUNT}
 aws ecs update-service --cluster ${CLUSTER} --region ${REGION} --service ${SERVICE_NAME} --force-new-deployment --task-definition ${FAMILY}:${REVISION} --desired-count ${DESIRED_COUNT}
